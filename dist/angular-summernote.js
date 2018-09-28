0e8de7ffd47f3a45318949211b70b262e2d7f220
jasonp <jasonp@isell.com.au>
2018-09-28 15:05:11 +0800

    build v0.8.2

--- dist/angular-summernote.js
+++ dist/angular-summernote.js
@@ -75,10 +75,12 @@ angular.module('summernote', [])
       var updateNgModel = function() {
         var newValue = element.summernote('code');
         if (element.summernote('isEmpty')) { newValue = ''; }
+        if (scope.ngRequired) { ngModel.$setValidity('required', !element.summernote('isEmpty')); }
         if (ngModel && ngModel.$viewValue !== newValue) {
-          $timeout(function() {
-            ngModel.$setViewValue(newValue);
-          }, 0);
+          ngModel.$setViewValue(newValue);
+          if (scope.$root && !scope.$root.$$phase) {
+            scope.$apply();
+          }
         }
       };
 
@@ -190,6 +192,7 @@ angular.module('summernote', [])
         summernoteConfig: '=config',
         editable: '=',
         editor: '=',
+        ngRequired: '=?ngRequired',
         disabled: '=',
         init: '&onInit',
         enter: '&onEnter',
